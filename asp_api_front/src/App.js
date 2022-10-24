0afa151952acf4ef37192eb80f5150555caecaf3
moisesph <mois.mp8@gmail.com>
2022-10-23 23:52:41 -0500

    Terminar formulario en react

--- asp_api_front/src/App.js
+++ asp_api_front/src/App.js
@@ -1,10 +1,59 @@
 import './App.css';
+import Nav from './components/nav/Nav.jsx';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [Users, setUsers] = useState([]);
+  const URL = "http://www.appusersasp.somee.com/api/usuario";
+
+  const eliminarUsuarios = async (e) => {
+    e.target.parentNode.display = "none";
+    await axios.delete(URL + '/' + e.target.value)
+      .then(res => res ? listarUsuarios() : console.log("Error al eliminar usuario, intente de nuevo"))
+      .catch(err => console.log(err));
+  }
+
+  const listarUsuarios = async ()=>{
+    let Users_temp;
+    await axios.get(URL).then(data => Users_temp = data.data).catch(err => console.log(err));
+    setUsers(Users_temp);
+  }
+
+  useEffect(()=>{
+    listarUsuarios();
+  }, []);
+
   return (
     <>
-      <h1>Api Usuarios</h1>
-      
+      <Nav />
+      <div className='UsersList'>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombres</th>
+              <th>Telefono</th>
+              <th>Correo</th>
+              <th>Ciudad</th>
+              <th>Fecha de Ingreso</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Users.map((value) => (
+              <tr key={value.ID_Usuario}>
+                <td>{value.Nombres}</td>
+                <td>{value.Telefono}</td>
+                <td>{value.Correo}</td>
+                <td>{value.Ciudad}</td>
+                <td>{value.FechaIngreso}</td>
+                <td><button value={value.ID_Usuario}>Editar</button></td>
+                <td><button onClick={eliminarUsuarios} value={value.ID_Usuario}>Eliminar</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
---
+++ asp_api_front/src/components/nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css';
+
+export default function Nav() {
+    return (
+        <>
+        <nav>
+            <h1>USUARIOS ASP.NET</h1>
+            <h2>API De aprendizaje usando ASP.NET y C#</h2>
+        </nav>
+        </>
+    )
+}
